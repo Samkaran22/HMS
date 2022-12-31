f8b453e01544cb34d9067289a557cf362ccac489
unknown <karankumarsk2209.com>
2022-12-31 16:44:59 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    if(strlen($name) <=8)
+    {
+        echo "Name should be more than 8 characters";
+    }
+}
